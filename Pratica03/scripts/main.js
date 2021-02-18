621e8bd21ea33b503cb3b81296788eecadb998d5
Gulliver Waldmann <gullivertadra@gmail.com>
2021-02-18 20:04:39 -0300

    Terceira atividade prática

---
+++ Pratica03/scripts/main.js
@@ -0,0 +1,37 @@
+// Gulliver Tadra Waldmann
+// RA: 17204126
+
+let minhaImagem = document.querySelector('img');
+
+minhaImagem.onclick = function() {
+    let meuSrc = minhaImagem.getAttribute('src');
+    if(meuSrc === 'images/firefox_logo.png') {
+      minhaImagem.setAttribute ('src','images/firefox2.jpg');
+    } else {
+      minhaImagem.setAttribute ('src','images/firefox_logo.png');
+    }
+}
+
+let meuBotao = document.querySelector('button');
+let meuCabecalho = document.querySelector('h1');
+
+function defineNomeUsuario() {
+    let meuNome = prompt('Por favor, digite seu nome.');
+    if(!meuNome || meuNome === null) {
+        defineNomeUsuario();
+    } else {
+        localStorage.setItem('nome', meuNome);
+        meuCabecalho.innerHTML = 'Mozilla é legal, ' + meuNome;
+    }
+
+    if(!localStorage.getItem('nome')) {
+        defineNomeUsuario();
+    } else {
+        let nomeGuardado = localStorage.getItem('nome');
+        meuCabecalho.textContent = 'Mozilla é legal, ' + nomeGuardado;
+    }  
+}
+
+meuBotao.onclick = function() { defineNomeUsuario();
+}
+
